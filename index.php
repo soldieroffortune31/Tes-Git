bbe6fbf6b61de1098134a23375888106b5cb463a
uzan310596 <uzan310596@yahoo.com>
2021-05-05 12:11:08 +0700

    Menambahkan CSS

--- index.php
+++ index.php
@@ -1,8 +1,25 @@
 <html>
 <head>
 	<title>Belajar Git</title>
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+	<script type="text/javascript" src="js/jquery.js"></script>
+	<script type="text/javascript" src="js/bootstrap.js"></script>
 </head>
 <body>
-Hello World
+	<table class="table table-bordered">
+		<thead class="table-dark">
+			<td>No.</td>
+			<td>Nama</td>
+			<td>Alamat</td>
+		</thead>
+		<tbody>
+			<td>1</td>
+			<td>Aliefsufi Uzan Kafil Ardi</td>
+			<td>Rondokuning</td>
+		</tbody>
+	</table>
+	<button class="btn btn-primary">
+		Click Here
+	</button>
 </body>
 </html>
